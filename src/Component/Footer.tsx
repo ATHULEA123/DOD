b703f3308f1c8523f9c483b020ace91f138d7795
athul <athul.stackup@gmail.com>
2025-03-19 14:05:17 +0530

    static

--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
                   REDEFINE YOUR SPACE?
                 </h1>
                 <p className="text-[#454A3F] md:text-lg text-sm font-medium">
-                  Let's Create Something Extraordinary Together!
+                  Lets Create Something Extraordinary Together!
                 </p>
                 <div>
                   <button className="flex items-center gap-2 bg-[#335555] text-white md:px-5 px-3 py-2 rounded-full text-[10px] md:text-lg">
