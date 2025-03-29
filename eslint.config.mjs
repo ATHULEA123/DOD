f6f363a4cc0b17c587613dd4709666bff49e0961
athul <athul.stackup@gmail.com>
2025-03-30 00:35:46 +0530

    djshakjhfdja

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,12 +14,17 @@
 // ];
 
 // export default eslintConfig;
-const eslintConfig = [
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "react/no-unescaped-entities": "off"
-    }
-  }
 ];
